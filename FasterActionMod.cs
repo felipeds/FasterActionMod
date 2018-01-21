b5ae7f44d5845b321ba4f1fddc5b33466c05ccfa
Felipe Demétrio de Souza <felipeds@gmail.com>
2018-01-21 13:34:08 -0500

    fix Bug with eating / Drinking

--- FasterActionMod.cs
+++ FasterActionMod.cs
@@ -2,6 +2,10 @@
 using UnityEngine;
 namespace FasterActionMod
 {
+    public class FasterConfig
+    {
+        public static float ACTION_TIME = 0.5f;
+    }
     [HarmonyPatch(typeof(AccelTimePopup), "SetActive")]
     public class FasterActionMod
     {
@@ -25,7 +29,7 @@ namespace FasterActionMod
     {
         public static void Prefix(ref float searchTimeSeconds)
         {
-            searchTimeSeconds = 0.01f;
+            searchTimeSeconds = 0.2f;
         }
 
     }
@@ -35,9 +39,31 @@ namespace FasterActionMod
     {
         public static void Prefix(ref float seconds)
         {
-            seconds = 0.5f;
+            seconds = FasterConfig.ACTION_TIME;
         }
 
     }
 
+    /// <summary>
+    /// For drinking and eating you have to set the right speed on the ADd actions so you eat / drink the right ammount on the accelerated time.
+    /// </summary>
+    [HarmonyPatch(typeof(Thirst), "AddThirstOverTime")]
+    public class FasterActionThirstMod
+    {
+        public static void Prefix(ref float timeSeconds)
+        {
+            timeSeconds = FasterConfig.ACTION_TIME;
+        }
+
+    }
+
+    [HarmonyPatch(typeof(Hunger), "AddReserveCaloriesOverTime")]
+    public class FasterActionHungerMod
+    {
+        public static void Prefix(ref float timeSeconds)
+        {
+            timeSeconds = FasterConfig.ACTION_TIME;
+        }
+
+    }
 }
