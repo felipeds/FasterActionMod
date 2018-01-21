f6548a3278effeb4e0ce32a3be36196457f5012b
Felipe Demétrio de Souza <felipeds@gmail.com>
2018-01-21 11:27:07 -0500

    Added searching and most of other actions

--- FasterActionMod.cs
+++ FasterActionMod.cs
@@ -17,8 +17,27 @@ namespace FasterActionMod
             }
         }
 
-        public static void Postfix()
-        {
-        }
     }
+
+
+    [HarmonyPatch(typeof(Container), "BeginContainerInteraction")]
+    public class FasterActionContainerUpdateMod
+    {
+        public static void Prefix(ref float searchTimeSeconds)
+        {
+            searchTimeSeconds = 0.01f;
+        }
+
+    }
+
+    [HarmonyPatch(typeof(Panel_GenericProgressBar), "Launch")]
+    public class FasterActionPanelLaunchMod
+    {
+        public static void Prefix(ref float seconds)
+        {
+            seconds = 0.5f;
+        }
+
+    }
+
 }
